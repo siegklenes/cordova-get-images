b31d45d8933b09a79a69cd53572147d7aa4b5aa2
Siegklenes Rolland Beulke <siegklenes.beulke@totvs.com.br>
2019-10-27 10:04:11 -0300

    fix

--- www/App.js
+++ www/App.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'App', 'coolMethod', [arg0]);
+    exec(success, error, 'ImageFolder', 'coolMethod', [arg0]);
 };
